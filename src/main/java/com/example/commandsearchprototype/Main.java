a644a247e79d3c3253d86838c13ca27f3f6cd32d
benalcocer <benalcocer7@gmail.com>
2025-02-19 00:52:47 -0800

    Fix issue with jar builds not being able to find resource files.

--- src/main/java/com/example/commandsearchprototype/Main.java
+++ src/main/java/com/example/commandsearchprototype/Main.java
@@ -3,6 +3,7 @@ package com.example.commandsearchprototype;
 import com.example.commandsearchprototype.util.CSVUtil;
 import com.example.commandsearchprototype.util.ResourceUtil;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -11,15 +12,14 @@ public class Main {
     public static final CommandHandler commandHandler = new CommandHandler();
 
     public static void main(String[] args) {
-        ResourceUtil.getResourceFile(Main.class, "commands.csv").ifPresent(file -> {
-            CSVUtil.readCSVFile(file, strings -> {
-                if (strings != null && strings.size() == 2) {
-                    Command newCommand = new Command();
-                    newCommand.setId(strings.get(0));
-                    newCommand.setDescription(strings.get(1).trim());
-                    allCommands.add(newCommand);
-                }
-            });
+        ResourceUtil.readResourceFileByLine(Main.class, "commands.csv", line -> {
+            List<String> strings = CSVUtil.parseCSVLine(line);
+            if (strings.size() == 2) {
+                Command newCommand = new Command();
+                newCommand.setId(strings.get(0));
+                newCommand.setDescription(strings.get(1).trim());
+                allCommands.add(newCommand);
+            }
         });
 
         MainApplication mainApplication = new MainApplication();
--- src/main/java/com/example/commandsearchprototype/util/ResourceUtil.java
+++ src/main/java/com/example/commandsearchprototype/util/ResourceUtil.java
@@ -1,20 +1,27 @@
 package com.example.commandsearchprototype.util;
 
-import java.io.File;
-import java.net.URL;
-import java.util.Optional;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.function.Consumer;
 
 public class ResourceUtil {
-    public static Optional<File> getResourceFile(Class<?> classType, String path) {
-        try {
-            URL url = classType.getResource(path);
-            File file = new File(url.getFile());
-            if (file.exists()) {
-                return Optional.of(file);
+    public static void readResourceFileByLine(Class<?> classType, String path, Consumer<String> lineConsumer) {
+        try (InputStream inputStream = classType.getResourceAsStream(path)) {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    try {
+                        lineConsumer.accept(line);
+                    } catch (Exception e) {
+                        // Uncaught exception in handler
+                        e.printStackTrace();
+                    }
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return Optional.empty();
     }
 }
